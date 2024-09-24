2fcc5ef2dcaf5aa3859a5064c0fe3ecfe40d28f0
= <=>
2024-09-24 14:16:41 +0200

    finalize notion member command

--- cogs/notion.py
+++ cogs/notion.py
@@ -16,14 +16,27 @@ class Notion(commands.Cog):
 
     @notion.subcommand(description="Obtenir les membres du Notion")
     async def membres(self, interaction: nc.Interaction):
+
         members = await self.notion_client.users.list()
-        print(members)
-        await interaction.send("Les membres sont affichés en console")
+
+        embed = nc.Embed(colour=0xffffff,
+                        title="Membres du Notion",
+                        url="https://living-jupiter-d3e.notion.site/Cours-et-Devoirs-fff109d1eb40810cb97cf5029bec4b1c")
+
+        for m in members["results"]:
+
+            if m["type"] != "person":
+                continue
+
+            embed.add_field(name=m["name"],value=f"*{m["person"]["email"]}*",inline=False)
+
+        embed.set_thumbnail("https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png")
+        await interaction.send(embed=embed)
 
 
 def setup(bot):
     load_dotenv()
     NOTION = getenv("TOKEN_NOTION")
-    client = notion_client.Client(auth=NOTION)
+    client = notion_client.AsyncClient(auth=NOTION)
     bot.add_cog(Notion(bot,client))
 
